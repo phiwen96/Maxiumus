d9c92ed9339d6a0e26984e176160d8952234b9e7
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2024-01-06 13:59:43 +0100

    a

--- apps/Learn.cpp
+++ apps/Learn.cpp
@@ -1,13 +1,13 @@
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <string>
 #include <string_view>
 #include <regex>
 #include <sstream>
 #include <signal.h>
 
-using std::cout, std::cin, std::endl, std::cerr, std::string, std::string_view, std::getline, std::ifstream, std::ofstream, std::fstream, std::regex, std::smatch;
+using std::cout, std::wcout, std::cin, std::wcin, std::endl, std::cerr, std::string, std::wstring, std::string_view, std::getline, std::ifstream, std::ofstream, std::fstream, std::regex, std::wregex, std::smatch, std::wsmatch, std::sregex_iterator, std::wsregex_iterator, std::istreambuf_iterator;
 using namespace std::filesystem;
 using namespace std::literals::string_literals;
 using namespace std::literals::string_view_literals; 
@@ -43,33 +43,94 @@ void sigint_handler(int sig) {
 
 
 auto main () -> int {
-	struct sigaction sa = {};
-	sa.sa_handler = sigint_handler;
-	sa.sa_flags = 0;
+	{
+		struct sigaction sa = {};
+		sa.sa_handler = sigint_handler;
+		sa.sa_flags = 0;
 
-	if (sigaction (SIGINT, &sa, NULL) == -1) {
-		cerr << "internal error >> sigaction" << endl;
-		exit(1);
+		if (sigaction (SIGINT, &sa, NULL) == -1) {
+			cerr << "internal error >> sigaction" << endl;
+			exit(1);
+		}
 	}
 
 	// saves.open (saves_file_name, std::ios_base::app);
+	// ifstream in{"table.txt"}; // input file
+	// if (!in)
+	// 	error("no input file\n");
 	saves.open (saves_file_name);
 	if (not saves.is_open ()) {
 		cerr << "internal error >> no save file named \"" << saves_file_name << "\"" << endl;
 		exit (0);
 	}
-	auto pattern = regex {R"(\[questions*\])"};
+
+	auto pattern = regex {R"(\[questions\])"};
 	auto matches = smatch {};
-	auto content = string {std::istreambuf_iterator<char> {saves}, std::istreambuf_iterator<char> {}};
+	auto const content = string {istreambuf_iterator<char> {saves}, istreambuf_iterator<char> {}};
 	saves.close ();
+	// cout << content << endl;
 	
-	auto iter = std::sregex_iterator {content.begin (), content.end (), pattern};
-	if (iter == std::sregex_iterator {}) {
-		cerr << "internal error >> no toml table with name \"questions\" found" << endl;
-		exit (1);
+	{
+		auto iter = sregex_iterator {content.begin (), content.end (), pattern};
+		if (iter == sregex_iterator {}) {
+			cerr << "internal error >> no toml table with name \"questions\" found" << endl;
+			exit (1);
+		}
 	}
-	pattern = regex {R"(\"\w*\")"};
-	iter = std::sregex_iterator {content.begin () + iter -> position (), content.end (), pattern};
+	// auto inp = string {};
+	// getline (cin, inp);
+	// auto patt = regex {R"([åäö]+)"};
+	pattern = regex {R"(\"+([åäöÅÄÖ,.\(\)\[\]/\-|]*\w*[åäöÅÄÖ,.\(\)\[\]/-|]*\w*\s*\?*)+\"+)"};
+	for (auto iter = sregex_iterator {content.begin (), content.end (), pattern}; iter != sregex_iterator {}; ++iter) {
+		cout << "hihihi" << endl;
+		cout << iter -> str () << endl;
+	}
+	// for (auto iter = sregex_iterator {inp.begin (), inp.end (), patt}; iter != sregex_iterator {}; ++iter) {
+	// 	cout << "match! " << iter -> str () << endl;
+	// }
+	// return 0;
+	// cout << iter -> str () << endl;
+
+	// pattern = regex {R"(\"{1,3}[\w\W]+\s*[\w\W]*\"{1,3})"};
+	// pattern = wregex {LR"(\"+(\w+\s*\w*\?*)+\"+)"};
+	// varför funkar inte regex på å ä ö ?
+	// pattern = wregex {LR"(\"+(\w*[åäöÅÄÖ]*\s*\w*[åäöÅÄÖ]*\?*)+\"+)"};
+	// auto pattern2 = regex {R"(\"+\w+\"+)"};
+	// pattern = "\"+w+\"+";
+	// cout << content << endl;
+	// iter = sregex_iterator {content.begin ()/* + iter -> position ()*/, content.end (), pattern};
+
+	// auto test_string = string {"\"Vad står OSI för?\" = \"Open Systems Interconnection\""};
+	// test_string = "\"hej\"";
+	// test_string = "\"hej jag?\"";
+	// cout << test_string << endl;
+	// auto pattern2 = regex {R"(\"\w+\s*\w*\?*\")"};
+	// auto matches2 = smatch {};
+	// saves.open(saves_file_name);
+	// if (regex_search (test_string, matches2, pattern2)) {
+	// 	for (auto i = 0; i < matches2.length (); ++i) {
+	// 		cout << "yay" << endl;
+	// 		cout << matches2 [i].str () << endl;
+	// 	}
+	// }
+		
+	// if (regex_match (content, matches2, pattern2)) {
+	// 	cout << "yay" << endl;
+	// 	for (auto i = 0; i < matches.length (); ++i) {
+	// 		cout << matches [i].str () << endl;
+	// 	}
+	// }
+	
+	// cout << iter -> str () << endl;
+	// for (auto iter = wsregex_iterator {content.begin (), content.end (), pattern}; iter != wsregex_iterator {}; ++iter) {
+	// 	cout << "hihihi" << endl;
+	// 	wcout << iter -> str () << endl;
+	// }
+	
+	// for (; iter != sregex_iterator {}; ++iter) {
+	// 	cout << iter -> prefix().str() << ":" << iter -> str () << endl;
+	// }
+	return 0;
 	// cout << iter -> str () << endl;
 	// if (regex_search (saves, matches, pattern)) {
 	// 	cout << "yay" << endl;
@@ -89,27 +150,47 @@ auto main () -> int {
 	cout << "Lets Learn!" << endl;
 	cout << "intro >> enter \"fastmode\" to create new questions" << endl;
 	cout << "intro >> enter \"learn\" to play" << endl;
-	MODE:
-	cout << "choose mode >> ";
-	cin >> input;
-	if (input == "fastmode") {
-		while (true) {
-			saves.open (saves_file_name, std::ios_base::app);
-			cout << "question >> ";
-			cin >> input;
-			saves << "\"" << input << "\"";
-			cout << "answer >> ";
-			cin >> input;
-			saves << " = \"" << input << "\"\n\n";
-			saves.close ();
-		};
-	} else if (input == "learn") {
-		cout << "error >> mode not yet implemented" << endl;
-		exit (1);
-	} else {
-		cout << "error >> no mode named \"" << input << "\"" << endl;
-		goto MODE;
+	// MODE:
+	// cout << "choose mode >> ";
+	// cin >> input;
+	// if (input == "fastmode") {
+
+	// auto const get_input = [&] () -> void {
+	// 	getline (cin, input);
+	// };
+
+	// while (true) {
+	// 	saves.open (saves_file_name, std::ios_base::app);
+	// 	cout << 
+	// }
+
+	while (true) {
+		saves.open(saves_file_name, std::ios_base::app);
+		QUESTION:
+		cout << "question >> ";
+		getline (cin, input);
+		if (input.length () == 0) {
+			goto QUESTION;
+		}
+		// cin >> input;
+		saves << "\"" << input << "\"";
+		ANSWER:
+		cout << "answer >> ";
+		getline (cin, input);
+		if (input.length () == 0) {
+			goto ANSWER;
+		}
+		// cin >> input;
+		saves << " = \"" << input << "\"\n\n";
+		saves.close();
 	}
+	// } else if (input == "learn") {
+	// 	cout << "error >> mode not yet implemented" << endl;
+	// 	exit (1);
+	// } else {
+	// 	cout << "error >> no mode named \"" << input << "\"" << endl;
+	// 	goto MODE;
+	// }
 	
 	// do {
 	// 	pattern = regex {""}
