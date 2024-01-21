c8c91e9e20aaa0f2540bff147f1f59fc130d8fda
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2024-01-21 14:58:03 +0100

    a

--- interfaces/Concurrency.cpp
+++ interfaces/Concurrency.cpp
@@ -3,6 +3,10 @@ module;
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <coroutine>
+#include <variant>
+#include <utility>
+#include <exception>
 
 export module Maximus.Concurrency;
 
@@ -48,11 +52,11 @@ private:
 
 static_assert (Mutex <mutex>);
 
-export template <Mutex Mtx = mutex>
+export template <typename T, Mutex Mtx = mutex>
 requires requires (Mtx mtx, pthread_cond_t& cv) {
 	mtx.wait (cv);
 }
-struct condition {
+struct condition : Mtx {
 	condition () {
 		pthread_cond_init (&handle, nullptr);
 	}
@@ -63,11 +67,15 @@ struct condition {
 		pthread_cond_signal (&handle);
 	}
 	auto wait () -> void {
-		mtx.wait (handle);
+		Mtx::wait (handle);
+	}
+	auto value () -> T& {
+		return val;
 	}
 private:
-	Mtx mtx;
+	// Mtx mtx;
 	pthread_cond_t handle;
+	T val;
 };
 
 static_assert (Condition <condition <mutex>>);
@@ -96,3 +104,78 @@ private:
 };
 
 static_assert (Thread <thread>);
+
+export template <typename T, template <typename> typename co_handle = std::coroutine_handle , bool continuation_on_final_suspend = true>
+struct co_promise {
+	auto final_suspend () noexcept {
+		// if constexpr (continuation_on_final_suspend) {
+		// 	return cont;
+		// } else {
+
+		// }
+ 		// return cont;
+	}
+private:
+	T res;
+	co_handle <co_promise> cont; // a reference to the coroutine waiting for the task to complete
+};
+
+export template <typename T>
+struct [[nodiscard]] co_task {
+	// using promise_type = co_promise <T, std::coroutine_handle, true>;
+	struct promise_type {
+		std::variant <std::monostate, T, std::exception_ptr> result_;
+		std::coroutine_handle <> continuation_; // A waiting coroutine
+		auto get_return_object () noexcept { 
+			return co_task {*this}; 
+		}
+		void return_value (T value) {
+			result_.template emplace <1> (std::move (value));
+		}
+		void unhandled_exception () noexcept {
+			result_.template emplace <2> (std::current_exception ());
+		}
+		auto initial_suspend () { 
+			return std::suspend_always {}; 
+		}
+		auto final_suspend () noexcept {
+			struct Awaitable {
+				bool await_ready () noexcept { 
+					return false; 
+				}
+				auto await_suspend (std::coroutine_handle <promise_type> h) noexcept {
+					return h.promise ().continuation_;
+				}
+				void await_resume() noexcept {
+
+				}
+			};
+			return Awaitable {};
+		}
+	};
+	explicit co_task (promise_type & p) noexcept : handle {std::coroutine_handle <promise_type>::from_promise (p)} {}
+	co_task (co_task&& t) noexcept : handle {std::exchange (t.handle, {})} {}
+	~co_task () {
+		if (handle) 
+			handle.destroy ();
+	}
+	// Awaitable interface
+	bool await_ready () { 
+		return false; 
+	}
+	auto await_suspend(std::coroutine_handle <> c) {
+		handle.promise ().cont = c;
+		return handle;
+	}
+	auto await_resume() -> T {
+		auto &result = handle.promise ().res;
+		if (result.index () == 1) {
+			return std::get <1> (std::move (result));
+		} else {
+			std::rethrow_exception (std::get<2> (std::move (result)));
+		}
+	}
+
+private:
+	std::coroutine_handle <T> handle;
+};
--- tests/Concurrency.cpp
+++ tests/Concurrency.cpp
@@ -1,12 +1,48 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <thread>
 
 import Maximus.Concurrency;
 
 // using std::cout, std::endl;
 
+auto test_0 () -> bool {
+	printf ("Test 0\n");
+
+	auto cond = condition <bool> {};
+	cond.value () = false;
+
+	auto const body0 = [&cond] {
+		cond.lock ();
+		printf ("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		cond.value () = true;
+		cond.wake ();
+		cond.unlock ();
+	};
+
+	auto const body1 = [&cond] {
+		cond.lock ();
+		while (not cond.value ()) {
+			cond.wait ();
+		}
+		printf ("Bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n");
+		cond.unlock ();
+	};
+
+	auto thr0 = std::thread {body1};
+	auto thr1 = std::thread {body0};
+
+	thr0.join ();
+	thr1.join ();
+
+	return true;
+}
+
 auto main (int argc, char** argv) -> int {
 	printf ("Testing Concurrency\n");
-	auto thr = thread {};
-	thr.join ();
+	if (not test_0 ()) {
+		printf ("failed test 0\n");
+		exit (1);
+	}
 	return 0;
 }
